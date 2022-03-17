c9fcaa057a3fe225c737036418ad070c8dbd57b5
V E L Δ <evan.robin07@gmail.com>
2022-03-18 00:58:30 +0100

    Optimization of the transparency
    Delete null-opacity pixels on textures, it can improve a little bit the performance and maximize the visibility on some maps for now until the multi-layering of transparent textures

--- LombaxToes/Shaders/standardunlit.frag.glsl
+++ LombaxToes/Shaders/standardunlit.frag.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-out vec4 colour;
+out vec4 frag;
 
 in vec2 UVs;
 
@@ -11,10 +11,23 @@ void main()
 {
 	if(useTexture)
 	{
+<<<<<<< Updated upstream
 		colour = texture(albedo, UVs);
 	}
 	else
 	{
 		colour = vec4(1.0, 0.0, 1.0, 1.0);
+=======
+		//vec4 texel = texture(albedo, UVs;
+		vec4 colour = texture(albedo, UVs);
+		if(colour.a == 0.0) discard;
+		frag = colour;
+	}
+	else
+	{
+		//vec4 texel = texture(noTextureTexture, UVs);
+		vec4 colour = vec4(1.0, 0.0, 1.0, 1.0);
+		frag = colour;
+>>>>>>> Stashed changes
 	}
 }
